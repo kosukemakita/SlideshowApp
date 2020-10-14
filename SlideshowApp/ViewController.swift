337c5c75c86719fbe6512ca82884daab716b3cf1
kosukemakita <makitakosuke0114@gmail.com>
2020-10-14 21:28:30 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 蒔田幸祐 on 2020/10/14.
+//  Copyright © 2020 kousuke.makita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
