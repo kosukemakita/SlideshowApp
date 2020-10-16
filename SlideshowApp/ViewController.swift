675a5098ca40951b775c98beb96f1cae8653d4ee
kosukemakita <makitakosuke0114@gmail.com>
2020-10-16 22:15:41 +0900

    4つの画像を追加

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var image1: UIImageView!
+    @IBOutlet weak var imageView: UIImageView!
     
     @IBAction func MoveToNext(_ sender: Any) {
     
@@ -22,7 +22,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         let image = UIImage(named: "sunset")
-        image1.image = image
+        let image2 = UIImage(named: "nightsky")
+        let image3 = UIImage(named: "nightsky2")
+        let image4 = UIImage(named: "aurora")
+        imageView.image = image
+        imageView.image = image2
+        imageView.image = image3
+        imageView.image = image4
     }
 
 
