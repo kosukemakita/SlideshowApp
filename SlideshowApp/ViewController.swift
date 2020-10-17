9c8f62ca94596762a86814d0e0a1da3452f19838
kosukemakita <makitakosuke0114@gmail.com>
2020-10-17 20:57:50 +0900

    ３つのボタンのコードまで書いた（動作せず）

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,29 +9,59 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var imageView: UIImageView!
     
-    @IBAction func MoveToNext(_ sender: Any) {
     
+    var nowIndex:Int = 0
+    var timer: Timer!
+    var imageArray:[UIImage] = [
+        UIImage(named: "sunset.jpeg")!,
+        UIImage(named: "nightsky.jpeg")!,
+        UIImage(named: "nightsky2.jpeg")!,
+        UIImage(named: "aurora.jpeg")!
+    ]
+    
+    
+    @IBAction func slideShowView(_ sender: Any) {
+        
+        if (timer == nil) {
+            
+            timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(changeImage), userInfo: nil, repeats: true)
+            
+        } else {
+            timer.invalidate()
+            timer = nil
+        }
+//        print(nowIndex)
     }
     
+    @IBAction func nextImage(_ sender: Any) {
+        changeImage()
+    }
+    @IBAction func previousImage(_ sender: Any) {
+        nowIndex -= 1
+        if (nowIndex == 0) {
+            nowIndex = imageArray.count
+        }
+        imageView.image = imageArray[nowIndex]
+    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        let image = UIImage(named: "sunset")
-        let image2 = UIImage(named: "nightsky")
-        let image3 = UIImage(named: "nightsky2")
-        let image4 = UIImage(named: "aurora")
-        imageView.image = image
-        imageView.image = image2
-        imageView.image = image3
-        imageView.image = image4
+        // Do any additional setup after loading the view
+        imageView.image = imageArray[nowIndex]
     }
-
-
-
+    
+    @objc func changeImage() {
+        nowIndex += 1
+        if (nowIndex == imageArray.count) {
+            nowIndex = 0
+        }
+        imageView.image = imageArray[nowIndex]
+        print(nowIndex)
+    }
+    
 }
 
