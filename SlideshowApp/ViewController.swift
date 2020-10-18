e7e5185d479e76bd2e5db4a662357b5e964964ed
kosukemakita <makitakosuke0114@gmail.com>
2020-10-18 16:56:22 +0900

    遷移先画面のボタン微調整

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     @IBAction func tapView(_ sender: Any) {
         print("タップされました")
     }
+//
+    
     
     @IBAction func unwind(_ segue: UIStoryboardSegue) {
     }
